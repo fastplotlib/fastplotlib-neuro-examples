1c82ca108247f2748dda7d201f662c178c048c62
kushalkolar <kushalkolar@gmail.com>
2026-05-02 09:30:22 -0400

    dmr multisession

--- examples/demixing_multi_session.py
+++ examples/demixing_multi_session.py
@@ -1,12 +1,58 @@
+from functools import partial
 from itertools import chain
 from pathlib import Path
+from typing import Sequence
 
 import masknmf
 import fastplotlib as fpl
 import numpy as np
+import pandas as pd
+import torch
 
-# from ibl import Video
-from masknmf_utils import ContoursManager
+session_df = pd.read_csv("./multisession-matching.csv")
+
+
+def get_coors(col_indices: torch.Tensor, shape: tuple[int, int]) -> torch.Tensor:
+    rows = col_indices // shape[1]
+    cols = col_indices % shape[1]
+    return torch.dstack([rows, cols]).squeeze()
+
+
+def master_to_local(session_id: int, index: Sequence[int]) -> Sequence[int | None]:
+    indices = list()
+    for i in index:
+        local_id = session_df.iloc[i][str(session_id)]
+
+        if np.isnan(local_id):
+            return None
+
+        indices.append(int(local_id))
+
+    return indices
+
+
+def image_clicked(session_id: int, centers: np.ndarray, ev):
+    col, row = ev.pick_info["index"]
+
+    local_index = np.argmin(
+        np.linalg.norm(centers - np.array([row, col]), axis=1)
+    )
+    # inverse map
+    index = np.argwhere(session_df[str(session_id)].values == local_index)
+    if index.size == 0:
+        print(f"index not in multi-session matching: {local_index}")
+        return
+
+    print(index)
+    index = index.item()
+
+    if "Shift" in ev.modifiers:
+        sv.append(index)
+    else:
+        sv.selection = [index]
+
+    for subplot in ndw_traces.figure:
+        subplot.auto_scale()
 
 
 parent_path = Path("/home/kushal/data/alyx/cortexlab/Subjects/")
@@ -15,7 +61,7 @@ subject = "SP058"
 
 dmrs = list()
 ref_ranges = dict()
-for i, session in enumerate(["2024-07-18", "2024-07-19", "2024-07-23"]):
+for session_index, session in enumerate(["2024-07-18", "2024-07-19", "2024-07-23"]):
     session_path = parent_path.joinpath(subject, session)
 
     dmr_path = session_path.joinpath(f"demix.hdf5")
@@ -37,72 +83,86 @@ for i, session in enumerate(["2024-07-18", "2024-07-19", "2024-07-23"]):
 
     ref_ranges.update(
         {
-            f"time-{i}": (dmr.timings[0], dmr.timings[-1], step),
+            f"time-{session_index}": (dmr.timings[0], dmr.timings[-1], step),
         }
     )
 
-extents = [
-    (0.00, 0.25, 0.75, 1.00),
-    (0.25, 0.50, 0.75, 1.00),
-    (0.50, 0.75, 0.75, 1.00),
-    (0.75, 1.00, 0.75, 1.00),
+subplot_names = [f"ac-{i}" for i in range(len(dmrs))]
 
-    (0.00, 0.25, 0.50, 0.75),
-    (0.25, 0.50, 0.50, 0.75),
-    (0.50, 0.75, 0.50, 0.75),
-    (0.75, 1.00, 0.50, 0.75),
 
-    (0.00, 0.25, 0.25, 0.50),
-    (0.25, 0.50, 0.25, 0.50),
-    (0.50, 0.75, 0.25, 0.50),
-    (0.75, 1.00, 0.25, 0.50),
-
-    # --- bottom full-width panel ---
-    (0.00, 1.00, 0.00, 0.25),
-]
-
-subplot_names = list(chain.from_iterable([f"pmd-{i}", f"ac-{i}", f"residual-{i}", f"fluctuating_background-{i}"] for i in range(len(dmrs))))
-subplot_names.extend(["traces"])
-
-ndw = fpl.NDWidget(
+ndw_images = fpl.NDWidget(
     ref_ranges,
-    extents=extents,
     names=subplot_names,
     controller_ids="sync",
-    shape=(3, 4),
-    size=(1200, 1200),
+    shape=(1, len(dmrs)),
+    size=(1800, 800),
 )
 
+ndw_traces = fpl.NDWidget(
+    ref_ranges,
+    ref_index=ndw_images.indices,
+    names=[f"traces-{i}" for i in range(len(dmrs))],
+    shape=(len(dmrs), 1),
+    size=(1800, 500),
+)
+
+sv = fpl.SelectionVector()
 ndgs = dict()
-for i, dmr in enumerate(dmrs):
-    for name in ["pmd", "ac", "residual", "fluctuating_background"]:
-        ndg = ndw[f"{name}-{i}"].add_nd_image(
-            getattr(dmr, f"{name}_array"),
-            (f"time-{i}", "m", "n"),
-            ("m", "n"),
-            index_mappings={f"time-{i}": dmr.timings},
-            name=f"{name}-{i}",
-        )
+# go through each session
+for session_index, dmr in enumerate(dmrs):
+    # add ac image
+    ndi = ndw_images[f"ac-{session_index}"].add_nd_image(
+        dmr.ac_array,
+        dims=(f"time-{session_index}", "m", "n"),
+        spatial_dims=("m", "n"),
+        slider_dim_transforms={f"time-{session_index}": dmr.timings},
+        name=f"ac-{session_index}",
+    )
 
-contours_manager = ContoursManager(
-    demixing_results=list(chain.from_iterable([dmr] * 4 for dmr in dmrs)),
-    subplots=[
-        ndw.figure[subplot_name]
-        for subplot_name in list(
-            chain.from_iterable(
-                [f"pmd-{i}", f"ac-{i}", f"residual-{i}", f"fluctuating_background-{i}"]
-                for i in range(len(dmrs))
-            )
-        )
-    ],
-)
+    # add traces
+    ndt = ndw_traces[f"traces-{session_index}"].add_nd_timeseries(
+        fpl.utils.heatmap_to_positions(dmr.c.T.cpu().numpy(), dmr.timings),
+        dims=("l", f"time-{session_index}", "d"),
+        spatial_dims=("l", f"time-{session_index}", "d"),
+        x_range_mode="fixed",
+        display_window=500,
+    )
 
+    contours = dmr.ac_array.contours
+    coors = list()
+    centers = np.empty((contours.shape[1], 2))
+
+    for k in range(contours.shape[1]):
+        c = get_coors(contours[:, k].coalesce().indices(), dmr.fov_shape).cpu().numpy()
+        coors.append(c)
+        centers[k] = c.mean(axis=0)
+
+    # add selectors
+    image_selector = fpl.ImageHighlightSelector(
+        lut="tab10",
+        lut_wrap="repeat",
+        selection_options={"pixels": coors},
+        options_color="w",
+        options_alpha=0.1,
+        alpha=0.7,
+    )
+    image_selector.add_graphic(ndi.graphic)
+    traces_visible_selector = fpl.VisibilitySelector(
+        ndt.graphic, lut="tab10", lut_wrap="repeat",
+    )
+    smap = partial(master_to_local, session_index)
+    sv.add_selector((image_selector, smap))
+    sv.add_selector((traces_visible_selector, smap))
+
+    ndi.graphic.add_event_handler(partial(image_clicked, session_index, centers), "double_click")
 
 cursor = fpl.Cursor()
 
-for subplot in ndw.figure:
+for subplot in ndw_images.figure:
     subplot.toolbar = False
     cursor.add_subplot(subplot)
 
-ndw.show()
+ndw_images.show()
+ndw_traces.show()
+
 fpl.loop.run()
